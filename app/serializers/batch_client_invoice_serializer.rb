14b1f68324acd571acbc5e79ebc52c06ac24ca41
sntgrdr <santiago.rodriguez@comunidadfeliz.cl>
2026-04-10 20:23:31 -0300

    docs: note client preload requirement in BatchClientInvoiceSerializer

--- app/serializers/batch_client_invoice_serializer.rb
+++ app/serializers/batch_client_invoice_serializer.rb
@@ -2,6 +2,7 @@ class BatchClientInvoiceSerializer < ActiveModel::Serializer
   attributes :id, :number, :date, :cae, :afip_authorized_at, :total_price,
              :client_name, :client_legal_number
 
+  # Callers must preload :client to avoid N+1 (see BatchInvoiceProcessDetailSerializer).
   def client_name
     object.client.legal_name
   end
